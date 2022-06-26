0f6d5fffa52666fb2ad270b17bc29c0888c8c131
Douglas Muth <dmuth@dmuth.org>
2022-06-26 11:51:44 -0400

    Wrote zip-search

--- lib/main.sh
+++ lib/main.sh
@@ -8,6 +8,10 @@
 #
 INDEX_DIR=""
 
+# The string to search for
+SEARCH_STRING=""
+
+
 #
 # Parse our command line arguments.
 #
@@ -52,6 +56,57 @@ function print_syntax() {
 	exit 1
 
 } # End of print_syntax()
+
+
+#
+# Parse our command line arguments.
+#
+function parse_args_search() {
+
+	if test ! "$2"
+	then
+		print_syntax_search
+	fi
+
+	SRC_DIR=$1
+	SRC_DIR=$(realpath ${SRC_DIR})
+
+	if test ! -d "${SRC_DIR}"
+	then
+		echo "! "
+		echo "! Directory '${SRC_DIR}' doesn't exist."
+		echo "! "
+		exit 1
+	fi
+
+	#
+	# Set our Index directory to be under the top-level directory of this app.
+	# Note that this file should have been included via a script that had pushd $(dirname $0) in it
+	# so that we changed to the directory where these scripts live.
+	#
+	INDEX_DIR=${PWD}/indexed-files/$(echo $SRC_DIR | sed -e "s|[^a-z0-9]|_|gi")
+
+	SEARCH_STRING=$2
+
+} # End of parse_args_search()
+
+
+#
+# Print our syntax and exit.
+#
+function print_syntax_search() {
+
+	echo "! "
+	echo "! Syntax: $0 SRC_DIR string"
+	echo "! "
+	echo "! SRC_DIR - The directory of our ZIP files.  This will affect which Index is searched."
+	echo "! "
+	echo "! string - The string to search for in the filename"
+	echo "! "
+	echo "! "
+	exit 1
+
+} # End of print_syntax_search()
 
 
 #
